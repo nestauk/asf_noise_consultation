081944efd8d98cd9972a52e3c84140e729913c5a
Daniel Lewis <daniel.lewis@nesta.org.uk>
2024-02-15 11:38:16 +0000

    Setup Nesta Data Science cookiecutter

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+"""Sphinx configuration."""
+
+from datetime import datetime
+
+
+project = "asf_noise_consultation"
+author = "Daniel Lewis"
+copyright = f"{datetime.now().year}, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    # "sphinx_click",
+    "sphinx_rtd_theme",
+]
+autodoc_typehints = "description"
+html_theme = "sphinx_rtd_theme"
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/asf_noise_consultation
+   License <license>
+
+asf_noise_consultation
+===============================
+
+Simple statistics for consultation response to DESNZ on heat pump noise.
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
